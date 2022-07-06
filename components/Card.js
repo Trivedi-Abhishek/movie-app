29c3f84ba80982bb2515c8ec574772eac8fbe515
Abhishek Trivedi <trivediabhishek622002@gmail.com>
2022-07-07 00:12:13 +0530

    complete the card component and make the websiteb responsive

--- components/Card.js
+++ components/Card.js
@@ -4,14 +4,14 @@ export default function Card({result}) {
     const BASE_URL="https://image.tmdb.org/t/p/original";
   return (
     
-    <div>
-        <Image layout="responsive" src={BASE_URL+result.backdrop_path} height={100} width={100}/>
+    <div className='p-3 hover:text-white active:text-red-400 xl:hover:scale-105 transition-transform duration-200'>
+        <Image className="cursor-pointer" layout="responsive" src={BASE_URL+result.backdrop_path} height={150} width={200}/>
 
-        <div>
+        <div className='cursor-pointer'>
             <p className='truncate'>{result.overview}</p>
             <p>{result.title || result.name}</p>
-            <p>{result.release_date || release.first_air_date}
-            <ThumbUpIcon className='h-6'/>
+            <p className='flex items-center'>{result.release_date || release.first_air_date}
+            <ThumbUpIcon className='h-6 ml-3 mr-2'/>
             {result.vote_count}
             </p>
         </div>
